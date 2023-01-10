27c84c99c45c217d750d707e882b3275be541a14
Lika <14dubinenkoanzhelika@gmail.com>
2023-01-11 00:06:55 +0200

    first part task10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  inputSearch: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.county-info'),
+};
+
+refs.inputSearch.addEventListener(
+  'input',
+  debounce(onInputSearch, DEBOUNCE_DELAY)
+);
+
+function onInputSearch(e) {
+  const name = e.target.value;
+  console.log(e);
+  const result = fetchCountries(name);
+  result.then(country => {
+    countryMarkup(country);
+  });
+  if (name === '') {
+    refs.countryList.innerHTML = '';
+  }
+}
+
+function countryMarkup(country) {
+  const markup = country
+    .map(elem => {
+      return `<li style="list-style:none">
+        <div style ="display:flex;gap:10px;align-items:center"><img src ="${
+          elem.flags.svg
+        }" alt ="${
+        elem.name.common
+      }" width="35px"><p style="font-size:25px;color:pink;margin:0">${
+        elem.name.official
+      }</p></div>
+        <p>
+          <b>Capital</b>: ${elem.capital}
+        </p>
+        <p>
+          <b>Population</b>: ${elem.population}
+        </p>
+        <p>
+          <b>Languages</b>: ${Object.values(elem.languages)}
+        </p>
+      </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+}
